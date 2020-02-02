144b16001bc77718c9b4f37367fb684c7bdc4a8c
twined quartz <fmorisato@gmail.com>
2020-02-02 09:02:58 +0900

    show enumerated number as prefix of sample name

--- src/showSampleList.js
+++ src/showSampleList.js
@@ -10,10 +10,10 @@ export default function showSampleList(state, response) {
         const sampleList = response["list_of_sample"];
         const sampleSelectDOM = document.querySelector("#rock_selector");
         sampleSelectDOM.innerHTML = "<option value='' disabled selected style='display:none;'>Select sample</option>";
-        const options = sampleList.map(v => {
+        const options = sampleList.map((v, i) => {
             const option = document.createElement("option")
             option.value = v["package-name"];
-            option.innerHTML = (savedSampleNames.includes(v["package-name"]) ? "✓ " : "") + v["list-name"][state.language]
+            option.innerHTML = (savedSampleNames.includes(v["package-name"]) ? "✓ " : "") + `${i + 1}. ${v["list-name"][state.language]}`
             if (savedSampleNames.includes(v["package-name"])) {
                 option.classList.add("downloaded")
             }
