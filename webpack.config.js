8ae70ba56203851611dd730864a63e94f5ae5dc0
pegmatite <fmorisato@gmail.com>
2020-04-22 23:38:30 +0900

    auto replace VERSION by package version in build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackStringReplacePlugin = require('html-webpack-string-replace-plugin');
+const VERSION = process.env.npm_package_version;
+
+
+module.exports = [{
     entry: `${__dirname}/src/index.js`,
     output: {
         path: `${__dirname}/js`,
@@ -6,4 +11,39 @@ module.exports = {
     },
     mode: "production",
     //mode: "development",
+
+
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            "template": "./src/index.html",
+            "filename": `${__dirname}/index.html`,
+        }),
+
+        new HtmlWebpackStringReplacePlugin({
+            '@VERSION@': VERSION,
+        })
+    ]
+},
+{
+    entry: `${__dirname}/src/sw/service_worker.js`,
+
+    output: {
+        path: `${__dirname}/`,
+        filename: "service_worker.js",
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'string-replace-loader',
+                options: {
+                    search: '@VERSION@',
+                    replace: VERSION,
+                }
+            }
+        ]
+    },
 }
+]
