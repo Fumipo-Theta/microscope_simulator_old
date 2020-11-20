2a187a5c5b8089392a66380d7e67cc53d3d3df15
pegmatite <fmorisato@gmail.com>
2020-11-20 22:38:24 +0900

    Fix impl

--- src/fetch_package_by_query.js
+++ src/fetch_package_by_query.js
@@ -1,11 +1,10 @@
 import { hideErrorMessage } from "./error_indicator_handler.js"
 import { showLoadingMessage } from "./loading_indicator_handler.js"
 import { hideWelcomeBoard, showViewer, showNicolButton } from "./viewer_handlers.js"
-import getPackageMetaData from "./getPackageMetaData.js"
+import queryImagePackage from "./queryImagePackage.js"
 import updateStateByMeta from "./updateStateByMeta.js"
 import updateViewDiscription from "./updateViewDiscription.js"
 import updateImageSrc from "./updateImageSrc.js"
-import fetchImagePackage from "./fetchImagePackage.js"
 import register from "./register.js"
 import markDownloadedOption from "./markDownloadedOption.js"
 import updateView from "./updateView.js"
@@ -21,19 +20,18 @@ export default function fetchPackageById(state, packageID) {
         showNicolButton()
 
         try {
-            const [response, isNewData, zipLoader] = await getPackageMetaData(state, packageID);
+            const [response, isNewData] = await queryImagePackage(state, packageID);
             const manifest = JSON.parse(response.manifest);
 
-            const [new_state, new_response] = await updateStateByMeta(state)(packageID, manifest)
+            const new_state = await updateStateByMeta(state)(packageID, manifest)
                 .then(updateViewDiscription)
                 .then(updateImageSrc(response.thumbnail, "jpg"))
                 .then(updateView)
-                .then(fetchImagePackage(zipLoader, response, isNewData))
 
             new_state.canRotate = true
 
-            updateImageSrc(new_response.zip, new_state.supportedImageType)(new_state)
-                .then(state => register(state, isNewData)(new_response))
+            updateImageSrc(response.zip, response.image_format)(new_state)
+                .then(state => register(state, isNewData)(response))
                 .then(markDownloadedOption(packageID)(manifest))
                 .then(updateView)
                 .then(res)
