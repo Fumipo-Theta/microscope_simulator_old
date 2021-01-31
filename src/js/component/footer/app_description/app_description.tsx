59de1f44df8201c764c99ba29774cffe8bb9f46f
pegmatite <fmorisato@gmail.com>
2021-01-31 15:36:42 +0900

    Introduce react from footer

---
+++ src/js/component/footer/app_description/app_description.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import { AppLogo } from '../../misc/app_logo/app_logo'
+
+type AppDescriptionProps = {
+    app_version: String
+}
+
+export const AppDescription: React.FC<AppDescriptionProps> = ({ app_version }) => {
+    return (
+        <>
+            <AppLogo />
+            <p>Version {app_version}</p>
+            <h1>Polarization microscope simulator, SCOPin rock !</h1>
+            <p><a href="./make_package.html">Create microscope images package</a></p>
+
+            <ul>
+                <li>This page and all contents are made by Fumipo Theta.
+                    Please do not copy, modify, and repost any contents without telling me.</li>
+                <li><a href="./about.html">Privacy policy</a></li>
+                <li>Source code is available at <a
+                    href="https://github.com/Fumipo-Theta/microscope_simulator/">GitHub</a></li>
+            </ul>
+        </>
+    )
+}
