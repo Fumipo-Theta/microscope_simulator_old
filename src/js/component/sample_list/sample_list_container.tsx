c7a4d9e082b83e408e8e52ef3ba18b07f7a3395a
pegmatite <fmorisato@gmail.com>
2021-09-09 21:34:40 +0900

    Impl load sample by hash and update hash by selected sample

--- src/js/component/sample_list/sample_list_container.tsx
+++ src/js/component/sample_list/sample_list_container.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { useState, useCallback, useEffect } from 'react'
 import { useLocation } from "react-router-dom"
 import { useRecoilValue, useSetRecoilState } from 'recoil'
-import { Language, QueryParams } from "@src/js/type/entity"
+import { Language, PackageId, QueryParams } from "@src/js/type/entity"
 import { SampleListItem, SampleList, SampleListKeys, SampleCategories, SampleCategoriesKeys, SampleListItemKeys } from "@src/js/type/sample"
 import { sampleListAppearanceState } from '@src/js/state/atom/sample_list_appearance_state'
 import { systemLanguageState } from '@src/js/state/atom/system_language_state'
@@ -11,7 +11,6 @@ import { sampleListNameState } from '@src/js/state/atom/sample_list_name_state'
 import { sampleListSelector } from '@src/js/state/atom/sample_list_state'
 import { SampleSelectorOption } from './sample_selector_option/sample_selector_option'
 import styles from "./index.module.css"
-import { selectedSampleNameState } from '@src/js/state/atom/selected_sample_name_state'
 
 type Props = {
 
@@ -51,6 +50,17 @@ const SampleCategoryContainer: React.FC<SampleCategories> = ({ [SampleCategories
 }
 
 const SampleListSelector: React.FC<SampleListSelectorProps> = ({ [SampleListKeys.ListOfSample]: listOfSample, lang }) => {
+    const location = useLocation()
+    useEffect(() => {
+        const preIndicatedSampleId = location.hash
+        console.log(`[info] sampleId is indicated by hash: ${preIndicatedSampleId}`)
+        if (listOfSample.length > 0 && preIndicatedSampleId && preIndicatedSampleId != '') {
+            const currentItem = searchSampleListItem(listOfSample, preIndicatedSampleId.slice(1))
+            setSelectedSampleListItemValue(currentItem)
+            setSelectedSampleIndex(currentItem?.[SampleListItemKeys.GlobalIndex])
+        }
+    }, [listOfSample])
+
     const setSelectedSampleListItemValue = useSetRecoilState(selectedSampleListItemState)
     const setSampleListAppearanceValue = useSetRecoilState(sampleListAppearanceState)
     const [selectedSampleIndex, setSelectedSampleIndex] = useState<number>()
@@ -58,7 +68,8 @@ const SampleListSelector: React.FC<SampleListSelectorProps> = ({ [SampleListKeys
         setSelectedSampleListItemValue(sample)
         setSelectedSampleIndex(index)
         setSampleListAppearanceValue(false)
-    }, [setSelectedSampleListItemValue])
+        window.location.hash = sample[SampleListItemKeys.PackageName]
+    }, [])
 
     return <div className={styles.sampleListSelector}>
         <div className={styles.sampleSelectorWrapper}>
@@ -79,7 +90,6 @@ const SampleListSelector: React.FC<SampleListSelectorProps> = ({ [SampleListKeys
 }
 
 export const SampleListContainer: React.FC<Props> = ({ }) => {
-    const location = useLocation()
     const { sample_list, category } = parseQueryParams(location.search)
     const setSampleListNameValue = useSetRecoilState(sampleListNameState)
     useEffect(() => {
@@ -99,4 +109,9 @@ export const SampleListContainer: React.FC<Props> = ({ }) => {
 
 const parseQueryParams = (queryString): QueryParams => {
     return queryString.substring(1).split('&').map((p) => p.split('=')).reduce((obj, e) => ({ ...obj, [e[0]]: e[1] }), {});
+}
+
+const searchSampleListItem = (sampleList: SampleListItem[], sampleId: PackageId): SampleListItem | null => {
+    const items = sampleList.filter(v => v[SampleListItemKeys.PackageName] == sampleId)
+    return items.length > 0 ? items[0] : null
 }
