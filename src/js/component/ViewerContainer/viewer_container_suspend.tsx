9f5caad14a855f26f21f1943ac214f3966c9b67c
pegmatite <fmorisato@gmail.com>
2021-10-11 12:39:29 +0900

    Fix fallback component

--- src/js/component/ViewerContainer/viewer_container_suspend.tsx
+++ src/js/component/ViewerContainer/viewer_container_suspend.tsx
@@ -2,14 +2,32 @@ import React from "react"
 import { useRecoilValue } from "recoil"
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { windowInnerSizeState } from "@src/js/state/atom/window_inner_size_state"
+import { NicolToggler } from "./nicol_toggler/nicol_toggler"
 import styles from "./index.module.css"
+import scaleScaleStyles from "@src/js/component/ViewerContainer/sample_scale/index.module.css"
+
+const dummyCanvasStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "5px",
+    borderRadius: "50%",
+    boxShadow: "2px 1px 4px #a0a0a0, -2px -1px 4px #ffffff",
+}
 
 export const ViewerContainerSuspend: React.FC = () => {
     const { width, height } = useRecoilValue(windowInnerSizeState)
     const viewerSize = getMaxViewerSize(width, height)
-    return <div className={styles.viewerLayerContainer} style={{ width: viewerSize, height: viewerSize }}>
-        <CircularProgress size={"5rem"} />
-    </div>
+    return <div className={styles.viewerLayerContainer} >
+        <div style={{ width: viewerSize, height: viewerSize, ...dummyCanvasStyle }}>
+            <CircularProgress size={"5rem"} />
+        </div>
+        <div className={scaleScaleStyles.scale} style={{ alignItems: "center", width: "50vw" }}>
+            <div className={scaleScaleStyles.scaleBar} style={{ width: "100%" }}></div>
+            <div className={scaleScaleStyles.scaleLabel}>Loading...</div>
+        </div>
+        <NicolToggler />
+    </div >
 }
 
 const getMaxViewerSize = (windowWidth, windowHeight) => {
