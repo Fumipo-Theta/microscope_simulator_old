a47dbca24c811a112ac5025d97375fbd4923d459
pegmatite <fmorisato@gmail.com>
2021-11-18 21:27:23 +0900

    Use custom social buttons

--- vender/app.config.ts
+++ vender/app.config.ts
@@ -1,7 +1,9 @@
 import { Footer } from "@vender/component/footer/custom/footer"
+import { Social } from "@vender/component/social/custom/social"
 
 const AppConfig = {
-    Footer: Footer
+    Footer: Footer,
+    Social: Social
 }
 
 export default AppConfig
