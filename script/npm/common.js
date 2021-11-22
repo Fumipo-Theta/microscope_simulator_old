96a2a6d72d66a5c4d18b55d52f59b4c7ec3c05f0
pegmatite <fmorisato@gmail.com>
2021-11-23 03:45:35 +0900

    Fix update script

--- script/npm/common.js
+++ script/npm/common.js
@@ -1,9 +1,17 @@
 const fs = require("fs-extra")
 
 const updatePackageJson = (current, coreJson) => {
+    const { new: _, ...scripts } = coreJson
+    const appOnlyScripts = {
+        "update:core": "node script/npm/update_core.js",
+    }
     return {
         ...current,
-        scripts: { ...current.scripts, ...coreJson.scripts },
+        scripts: {
+            ...current.scripts,
+            ...coreJson.scripts,
+            ...appOnlyScripts,
+        },
         devDependencies: { ...current.devDependencies, ...coreJson.devDependencies },
         dependencies: { ...current.dependencies, ...coreJson.dependencies },
     }
